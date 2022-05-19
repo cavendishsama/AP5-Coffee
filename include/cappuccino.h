b320dbe874207040dbee13ac8a4febb7e8c951e3
cavendishsama <alibabaloo.aut@gmail.com>
2022-05-19 20:09:23 +0430

    cappuccino is modified

--- include/cappuccino.h
+++ include/cappuccino.h
@@ -1,4 +1,30 @@
 #ifndef CAPPUCCINO
 #define CAPPUCCINO
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include "espresso_based.h"
+#include "ingredient.h"
+#include "sub_ingredients.h"
+
+class Cappuccino : public EspressoBased 
+{
+public:
+    Cappuccino();
+    Cappuccino(const Cappuccino& cap);
+    ~Cappuccino();
+    void operator=(const Cappuccino& cap);
+
+    virtual std::string get_name();
+    virtual double price();
+
+    void add_side_item(Ingredient* side);
+    std::vector<Ingredient*>& get_side_items();
+
+private:
+    std::vector<Ingredient*> side_items;
+};
+
 #endif // CAPPUCCINO
