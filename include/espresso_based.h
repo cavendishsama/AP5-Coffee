7232aa6b89a2d66d059878afdd96e5b49d1ac153
cavendishsama <alibabaloo.aut@gmail.com>
2022-05-19 20:10:14 +0430

    espresso base class is modified

--- include/espresso_based.h
+++ include/espresso_based.h
@@ -1,4 +1,33 @@
 #ifndef ESPRESSO_BASED_H
 #define ESPRESSO_BASED_H
 
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<string>
+#include"ingredient.h"
+#include"sub_ingredients.h"
+
+class EspressoBased
+{
+public:
+    virtual std::string get_name() = 0;
+    virtual double price() = 0;
+
+    void brew();
+    std::vector<Ingredient*>& get_ingredients();
+
+    ~EspressoBased();
+
+protected:
+    EspressoBased() = default;
+    EspressoBased(const EspressoBased& esp);
+    void operator=(const EspressoBased& esp);
+
+    std::vector<Ingredient*> ingredients;
+    std::string name;
+
+};
+
 #endif // ESPRESSO_BASED_H
