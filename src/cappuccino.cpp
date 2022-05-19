d0a359b4106816e4ae63525361e8add144d85ba3
cavendishsama <alibabaloo.aut@gmail.com>
2022-05-19 20:11:29 +0430

    all functions are completed except for copy constructor and =operator

--- src/cappuccino.cpp
+++ src/cappuccino.cpp
@@ -1,1 +1,44 @@
 #include "cappuccino.h"
+
+Cappuccino::Cappuccino() : side_items {}{
+    name = "Cappuccino";
+    ingredients.push_back(new Espresso(2));
+    ingredients.push_back(new Milk(2));
+    ingredients.push_back(new MilkFoam(1));
+}
+
+Cappuccino::Cappuccino(const Cappuccino& cap){
+    
+}
+
+Cappuccino::~Cappuccino()
+{
+    for(const auto& i : side_items)
+        delete i;
+    side_items.clear();
+}
+
+void Cappuccino::operator=(const Cappuccino& cap){
+
+}
+
+std::string Cappuccino::get_name(){
+    return name;
+}
+
+double Cappuccino::price(){
+    double _price {};
+    for(const auto& j : ingredients)
+        _price += j->price();
+    for(const auto& i : side_items)
+        _price += i->price();
+    return _price;
+}
+
+void Cappuccino::add_side_item(Ingredient* side){
+    side_items.push_back(side);
+}
+
+std::vector<Ingredient*>& Cappuccino::get_side_items(){
+    return side_items;
+}
