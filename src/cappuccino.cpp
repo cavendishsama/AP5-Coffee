f5e07e9c4f80a740a8b8c05d5b65028b9fe77b3c
cavendishsama <alibabaloo.aut@gmail.com>
2022-05-20 20:08:30 +0430

    both copy constructor and =operator are added and modified

--- src/cappuccino.cpp
+++ src/cappuccino.cpp
@@ -7,8 +7,35 @@ Cappuccino::Cappuccino() : side_items {}{
     ingredients.push_back(new MilkFoam(1));
 }
 
-Cappuccino::Cappuccino(const Cappuccino& cap){
-    
+Cappuccino::Cappuccino(const Cappuccino& cap) : EspressoBased(cap) {
+
+    auto temp {const_cast<Cappuccino&>(cap).get_side_items()};   //constcast bokon
+    std::vector<std::string> v {"Cinnamon","Chocolate","Sugar","Cookie", \
+                                "Espresso","Milk","MilkFoam","Water"};
+
+    for(const auto& i : temp){
+        size_t flag {};
+        for(const auto& j : v){
+            flag++;
+            if(i->get_name() == j)
+                break;
+        }
+
+        switch (flag)   //i'll explain what's going on in this section but i used flag to check which ingredient i'm
+                        //adding to ingredinets vector (check the order of vector v and compare it to cases)
+        {
+        case 1: this->add_side_item(new Cinnamon(i->get_units()));    break;
+        case 2: this->add_side_item(new Chocolate(i->get_units()));   break;
+        case 3: this->add_side_item(new Sugar(i->get_units()));       break;
+        case 4: this->add_side_item(new Cookie(i->get_units()));      break;
+        case 5: this->add_side_item(new Espresso(i->get_units()));    break;
+        case 6: this->add_side_item(new Milk(i->get_units()));        break;
+        case 7: this->add_side_item(new MilkFoam(i->get_units()));    break;
+        case 8: this->add_side_item(new Water(i->get_units()));       break;
+        default:
+            break;
+        }
+    }
 }
 
 Cappuccino::~Cappuccino()
@@ -19,7 +46,34 @@ Cappuccino::~Cappuccino()
 }
 
 void Cappuccino::operator=(const Cappuccino& cap){
+    ingredients.clear();
+    auto temp {const_cast<Cappuccino&>(cap).get_ingredients()};   //constcast bokon
+    std::vector<std::string> v {"Cinnamon","Chocolate","Sugar","Cookie", \
+                                "Espresso","Milk","MilkFoam","Water"};
 
+    for(const auto& i : temp){
+        size_t flag {};
+        for(const auto& j : v){
+            flag++;
+            if(i->get_name() == j)
+                break;
+        }
+
+        switch (flag)   //i'll explain what's going on in this section but i used flag to check which ingredient i'm
+                        //adding to ingredinets vector (check the order of vector v and compare it to cases)
+        {
+        case 1: this->add_side_item(new Cinnamon(i->get_units()));    break;
+        case 2: this->add_side_item(new Chocolate(i->get_units()));   break;
+        case 3: this->add_side_item(new Sugar(i->get_units()));       break;
+        case 4: this->add_side_item(new Cookie(i->get_units()));      break;
+        case 5: this->add_side_item(new Espresso(i->get_units()));    break;
+        case 6: this->add_side_item(new Milk(i->get_units()));        break;
+        case 7: this->add_side_item(new MilkFoam(i->get_units()));    break;
+        case 8: this->add_side_item(new Water(i->get_units()));       break;
+        default:
+            break;
+        }
+    }
 }
 
 std::string Cappuccino::get_name(){
